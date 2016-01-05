09b21b1e2a5828d6ae70d32795bf0593cd78a391
manol <manol@indinet.es>
2016-01-05 20:54:56 +0100

    method logout

--- login.js
+++ login.js
@@ -42,6 +42,20 @@ angular.module('manviny.dflogin', [
 			return deferred.promise;
 		};
 
+		this.logout = function (options) {
+			var deferred = $q.defer();
+			
+	          $http({
+	            method: 'DELETE',
+	            url: '/api/v2/user/session'
+	          }).success(function () {
+	            delete $http.defaults.headers.common['X-DreamFactory-Session-Token'];
+	            deferred.resolve();
+	          });
+
+			return deferred.promise;
+		};
+
 		this.register = function (options) {
 			var deferred = $q.defer();
 			
@@ -76,6 +90,23 @@ angular.module('manviny.dflogin', [
 	}
 ])
 
+.controller('LogoutCtrl', [ 
+  '$scope', '$LoginHelper', '$location', '$rootScope',
+
+  function ($scope, $LoginHelper, $location, $rootScope) {
+    	$rootScope.isLoggedIn = true;
+    
+
+        $scope.logout = function () {
+			LoginHelper.logout({ })
+			.then(function () {
+				$rootScope.isLoggedIn = false;
+				// $location.path('/contacts');
+			});
+        };
+  }
+
+])
 
 .controller('RegisterController', [
 	'$scope', 'LoginHelper', '$location', '$rootScope',
