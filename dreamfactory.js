73aa20d8ed63fbfc6145a1b1414303da28e5b4dd
manviny <manol@indinet.es>
2016-02-05 19:45:12 +0100

    create

--- dreamfactory.js
+++ dreamfactory.js
@@ -307,7 +307,7 @@
 		*/
 		this.createFolder = function (path, name) {
 			var deferred = $q.defer();
-			$http.post('/api/v2/S3'+ path.replace(/^\/|\/$/g, '') +'/' + name +'/' ).then(function (result) {
+			$http.post('/api/v2/S3/'+ path.replace(/^\/|\/$/g, '') +'/' + name +'/' ).then(function (result) {
 				 deferred.resolve(result.data);
 			}, deferred.reject);
 			return deferred.promise;
@@ -352,7 +352,7 @@
 		    var fd = new FormData();
 		    fd.append("files", file);
 
-		    $http.post( "/api/v2/S3/"+ path  + '/' + file.name, fd, {	
+		    $http.post( "/api/v2/S3/"+ path.replace(/^\/|\/$/g, '')  + '/' + file.name, fd, {	
 		        headers: {'Content-Type': undefined },
 		        transformRequest: angular.identity
 		    })  
