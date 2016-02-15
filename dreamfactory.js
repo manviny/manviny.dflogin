3c2ddd715b07378f9873162abe3c443ee9ac997c
manviny <manol@indinet.es>
2016-02-15 22:23:23 +0100

    +

--- dreamfactory.js
+++ dreamfactory.js
@@ -99,13 +99,25 @@
 	            
 	            element.bind('change', function(){
 	                scope.$apply(function(){
-	                    modelSetter(scope, element[0].files[0]);
+	                    modelSetter(scope, element[0].files);
 	                });
 	            });
 	        }
 	    };
 	}])
-    
+
+	.directive('fileChange', [
+	    function() {
+	        return {
+	            link: function(scope, element, attrs) {
+	                element[0].onchange = function() {
+	                    scope[attrs['fileChange']](element[0])
+	                }
+	            }
+	            
+	        }
+	    }
+	])    
     
 	/**
      * @memberof manviny	
@@ -144,6 +156,17 @@
 			}, deferred.reject);
 			return deferred.promise;
 		};
+
+   //register new user
+	//    this.register = function () {
+	//     var deferred = $q.defer();
+	//     $http.post('/api/v2/user/register?login=true', options).then(function (result) {
+	//      handleResult(result)
+	//      deferred.resolve(result.data);
+	//     }, deferred.reject);
+	//     return deferred.promise;
+	//    }
+
 		/**
 		* logout user
 		* @memberof DFUser
@@ -157,9 +180,15 @@
       			method: 'DELETE',
       			url: '/api/v2/user/session'
       		}).success(function (result) {
+      			$rootScope.user = '';
       			delete $http.defaults.headers.common['X-DreamFactory-Session-Token'];
       			deferred.resolve(result);
-      		});
+      		})
+      		.error(function(data){
+      			$rootScope.user = '';
+      			delete $http.defaults.headers.common['X-DreamFactory-Session-Token'];      			
+		    	deferred.reject(data);
+			});	
 			return deferred.promise;
 		};
 
@@ -212,9 +241,9 @@
 	 * @description
 	 *   Services to use S3
 	 */     
-	.service('DFS3', [ '$http', '$q', '$rootScope', function ($http, $q, $rootScope) {
+	.service('DFS3', [ '$http', '$q', '$rootScope','BUCKET', function ($http, $q, $rootScope, BUCKET) {
 
-
+		var userPath = '/api/v2/'+ BUCKET + '/';
 		/**
 		* Get bucket files and folders from the given path (1 level, not recursive)
 		* @memberof DFS3
@@ -227,17 +256,57 @@
 		*			.then(function (result) { 		
 		*/
 		this.getBucketContent = function (path) {
+			console.debug("usuario", $rootScope.user)
 			var files = []; var folders = [];
 			if(path==undefined) path = '';
 
 			var deferred = $q.defer();
-			$http.get('/api/v2/S3/'+ path.replace(/^\/|\/$/g, '') +'/?include_folders=true&include_files=true').then(function (result) {
+			$http.get(userPath + path.replace(/^\/|\/$/g, '') +'/?include_folders=true&include_files=true').then(function (result) {
 				angular.forEach(result.data.resource, function(value) {
-					if(value.type=='folder'){ folders.push(value) }
-					else { files.push(value) }
+					if (value.name.charAt(0)!='.'){							// fichero oculto
+						if(value.type=='folder'){ folders.push(value) }
+						else { files.push(value) }
+					}
 				});
 				deferred.resolve({files:files, folders:folders});
 			}, deferred.reject);
+			return deferred.promise;
+		};
+		/**
+		* Get bucket files and folders from the given path (1 level, not recursive)
+		* @memberof DFS3
+	 	* @function getBucketContent	 		
+		* @param {path}  path from where to get content
+		* @returns {array} array of Objects => {files:files, folders:folders} -> (content_length, content_type, last_modified, name, path, type)
+	    * @example
+	    *   Usage:
+	    *   		DFS3.getBucketContent( || '' || '/' || 'path'|| '/path'|| 'path/' || '/path/')
+		*			.then(function (result) { 		
+		*/
+		this.getS3Content = function (bucket, path) {
+			console.debug("usuario", $rootScope.user)
+			var files = []; var folders = [];
+			if(path==undefined) path = '';
+
+			var deferred = $q.defer();
+	  		$http({
+	  			method: 'POST',
+	  			// url: 'http://indinet.es/aws/getBucket/',
+	  			url: 'http://indinet.es/aws/listBuckets/',
+	  			data: {'role':'jrc', 'bucket':bucket, 'prefix': path}
+	  		}).success(function (result) {
+	  			console.log("KKKKK",result)
+				angular.forEach(result, function(value) {
+					if (value.Key.charAt(0)!='.'){							// fichero oculto
+						if(value.Key.slice(-1)=='/'){ folders.push(value) }
+						else { files.push(value) }
+					}
+				});
+	  			deferred.resolve({files:files, folders:folders});
+	  		})
+	  		.error(function(data){
+	  			deferred.reject
+			});	
 			return deferred.promise;
 		};
 
@@ -258,27 +327,68 @@
 			var deferred = $q.defer();
 
 			// quita primer y ultimo '/'
-			$http.get('/api/v2/S3/'+ path.replace(/^\/|\/$/g, '') +'/?as_list=true&as_access_list=true').then(function (result) {
+			$http.get(userPath + path.replace(/^\/|\/$/g, '') +'/?as_list=true&as_access_list=true')
+		    .success(function(result){
 				angular.forEach(result.data.resource, function(value) {
 					if(value.slice(-1)!='*') paths.push(value.slice(0, -1));				// quita rutas que terminan en *
 				});
-				deferred.resolve(paths);
-			}, deferred.reject);
+		    	deferred.resolve(paths);
+			})
+			.error(function(data){
+		    	deferred.reject(data);
+			});	
+
+		
 			return deferred.promise;
 		};
 
 		/**
-		* creates EMPTY file in S3
+		* creates FILE in S3, its content is data
 		* @memberof DFS3
 	 	* @function createFile	 		
 		* @param {path,name} path in S3, name of the file
 		* @returns {Hash} filterd attributes
 		*/
-		this.createFile = function (path, name) {
+		this.createFile = function (path, file, data) {
 			var deferred = $q.defer();
-			$http.post('/api/v2/S3'+ path.replace(/^\/|\/$/g, '') +'/' + name).then(function (result) {
+			$http.post( this.getPath(path, file), data ).then(function (result) {
 				 deferred.resolve(result.data);
 			}, deferred.reject);
+			return deferred.promise;
+		};
+
+		/**
+		* get  FILE from S3
+		* @memberof DFS3
+	 	* @function getFile	 		
+		* @param {path,name} path in S3, name of the file
+		* @returns {Hash} filterd attributes
+		*/
+		this.getFile = function (path, file, download) {
+			// if(download) download = '?download=true';
+			download = download ? '?download=true' : '';
+			console.debug("DOWNLOAD", this.getPath(path, file)+ download );
+			var deferred = $q.defer();
+
+
+			$http.get( this.getPath(path, file)+ download )
+			.then(function (result) {
+				if(download!='') {			// Descargar ichero
+				    var anchor = angular.element('<a/>');
+				    anchor.attr(
+				    {
+				        href: 'data:text/html;charset=utf-8,' + (result),
+				        // href: 'data:image/png,' + encodeURI(result.data),
+				        target: '_blank',
+				        download: file
+				    }
+				    )[0].click();						
+				}
+				// Devolver metadata
+				deferred.resolve(result.data);
+			}, deferred.reject);
+
+
 			return deferred.promise;
 		};
 
@@ -290,8 +400,8 @@
 		* @param {path,name} path in S3, name of the file
 		* @returns {Hash} filterd attributes
 		*/
-		this.deleteFile = function (path, name) {
-			
+		this.deleteFile = function (path, file) {
+			console.log(this.getPath(path, file))
 			var deferred = $q.defer();
 			$http.delete( this.getPath(path, file) ).then(function (result) {
 				 deferred.resolve(result.data);
@@ -306,7 +416,7 @@
 		* @param {path,name} path in S3, name of the file
 		* @returns {Hash} filterd attributes
 		*/
-		this.createFolder = function (path, name) {
+		this.createFolder = function (path, file) {
 			var deferred = $q.defer();
 			$http.post( this.getPath(path, file) + '/' ).then(function (result) {
 				 deferred.resolve(result.data);
@@ -372,7 +482,7 @@
 		// calula el path 
 		this.getPath = function (path, name) {
 			console.debug(path, name);
-			var url = '/api/v2/S3/';
+			var url = userPath;
 			if(path=='/' || path=='') { url = url + name }
 			else { url = url + path.replace(/^\/|\/$/g, '')  + '/' + name}
 			console.debug(url);
