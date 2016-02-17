61bda00f1583038ce599f8b742d3268b345e3489
manviny <manol@indinet.es>
2016-02-17 08:50:49 +0100

    +

--- dreamfactory.js
+++ dreamfactory.js
@@ -181,12 +181,160 @@
 	  			url: 'http://indinet.es/aws/listobjects/',
 	  			data: {'prefix': path}
 	  		})
-	  		.success(function (result) { deferred.resolve(result); })
-	  		.error(function(data){ deferred.reject; });	
+	  		.success(function (result) { deferred.resolve(result);alert("j2") })
+	  		.error(function(data){ deferred.reject; alert("j3")});	
 			return deferred.promise;
 		}
 
+///////////////////////////////////////////////////
+//					ADAPTAR
+///////////////////////////////////////////////////
+		/**
+		* creates FILE in S3, its content is data
+		* @memberof DFS3
+	 	* @function createFile	 		
+		* @param {path,name} path in S3, name of the file
+		* @returns {Hash} filterd attributes
+		*/
+		this.createFile = function (path, file, data) {
+			var deferred = $q.defer();
+			$http.post( this.getPath(path, file), data ).then(function (result) {
+				 deferred.resolve(result.data);
+			}, deferred.reject);
+			return deferred.promise;
+		};
 
+		/**
+		* get  FILE from S3
+		* @memberof DFS3
+	 	* @function getFile	 		
+		* @param {path,name} path in S3, name of the file
+		* @returns {Hash} filterd attributes
+		*/
+		this.getFile = function (path, file, download) {
+			// if(download) download = '?download=true';
+			download = download ? '?download=true' : '';
+			console.debug("DOWNLOAD", this.getPath(path, file)+ download );
+			var deferred = $q.defer();
+
+
+			$http.get( this.getPath(path, file)+ download )
+			.then(function (result) {
+				if(download!='') {			// Descargar ichero
+				    var anchor = angular.element('<a/>');
+				    anchor.attr(
+				    {
+				        href: 'data:text/html;charset=utf-8,' + (result),
+				        // href: 'data:image/png,' + encodeURI(result.data),
+				        target: '_blank',
+				        download: file
+				    }
+				    )[0].click();						
+				}
+				// Devolver metadata
+				deferred.resolve(result.data);
+			}, deferred.reject);
+
+
+			return deferred.promise;
+		};
+
+
+		/**
+		* deletes  file in S3
+		* @memberof DFS3
+	 	* @function deleteFile	 		
+		* @param {path,name} path in S3, name of the file
+		* @returns {Hash} filterd attributes
+		*/
+		this.deleteFile = function (path, file) {
+			console.log(this.getPath(path, file))
+			var deferred = $q.defer();
+			$http.delete( this.getPath(path, file) ).then(function (result) {
+				 deferred.resolve(result.data);
+			}, deferred.reject);
+			return deferred.promise;
+		};
+
+		/**
+		* creates FOLDER file in S3
+		* @memberof DFS3ΩΩΩ
+	 	* @function createFolder	 		
+		* @param {path,name} path in S3, name of the file
+		* @returns {Hash} filterd attributes
+		*/
+		this.createFolder = function (path, file) {
+			var deferred = $q.defer();
+			$http.post( this.getPath(path, file) + '/' ).then(function (result) {
+				 deferred.resolve(result.data);
+			}, deferred.reject);
+			return deferred.promise;
+		};
+
+
+		/**
+		* converts path to Breadcrumbs
+		* @memberof DFS3
+	 	* @function pathToBreadcrumb	 		
+		* @param {path,name} path in S3, name of the file
+		* @returns {Hash} filterd attributes
+		*/
+		this.pathToBreadcrumbs = function (path) {
+			
+			var breadcrumbs = path.split('/');
+			breadcrumbs.pop(); 
+			return breadcrumbs;
+		};
+
+		/**
+		* converts Breadcrumbs to path 
+		* @memberof DFS3ΩΩΩ
+	 	* @function breadcrumbToPath	 		
+		* @param {path,name} path in S3, name of the file
+		* @returns {Hash} filterd attributes
+		*/
+		this.breadcrumbsToPath = function (name) {
+
+		};
+
+		/**
+		* Uploads a file to S3
+		* @memberof DFS3
+	 	* @function uploadFile	 		
+		* @param {path,name} path in S3, name of the file
+		* @returns {Hash} filterd attributes
+		*/
+		this.uploadFile = function (path, file) {
+
+			var deferred = $q.defer();
+
+		    var fd = new FormData();
+		    fd.append("files", file);
+
+		    $http.post( this.getPath(path, file.name) , fd, {	
+		        headers: {'Content-Type': undefined },
+		        transformRequest: angular.identity
+		    })  
+		    .success(function(data){
+		    	deferred.resolve(data);
+			})
+			.error(function(data){
+		    	deferred.reject(data);
+			});	
+
+			return deferred.promise;
+
+		};
+
+		// calula el path 
+		this.getPath = function (path, name) {
+			console.debug(path, name);
+			var url = userPath;
+			if(path=='/' || path=='') { url = url + name }
+			else { url = url + path.replace(/^\/|\/$/g, '')  + '/' + name}
+			console.debug(url);
+			return url;
+		};
 
 
 
